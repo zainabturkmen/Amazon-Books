d03e1da74936909e3923be5c75f3901c46854a1d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-16 14:58:56 +0430

    adding some changes

--- src/index.js
+++ src/index.js
@@ -13,4 +13,4 @@ const Greeting = () => {
   return React.createElement("h1", {}, "hello world")
 };
 
-ReactDOM.render(<Greeting/>, document.getElementById("root"))
+ReactDOM.render(<Greeting/>, document.getElementById("root"));
