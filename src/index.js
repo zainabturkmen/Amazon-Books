3130fb01962dc20c1d76dbfdebf2bc3add37a627
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-16 23:01:49 +0430

    adding some changes

--- src/index.js
+++ src/index.js
@@ -13,8 +13,19 @@ function BookList(){
 
 const Book = () => {
   return <article>
-    this is a book
+    <Image />
   </article>
 }
 
+const Image = () => {
+  return (
+    <img src="https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL600_SR600,400_.jpg" />
+  )
+};
+
+const Title = () => {
+  return(
+    <h1>I Love You To The Moon And Back</h1>
+  )
+};
 ReactDOM.render(<BookList />, document.getElementById("root"));
