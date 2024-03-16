7b2352dd6184fb7ddf6a84c8934c683ad3757ccd
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-16 15:45:54 +0430

    adding some changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,30 @@ import ReactDOM from "react-dom";
 
 function Greeting(){
   return (<>
-   <h1 className="Hehe">hello world</h1>
-   <a href="#">Hey all</a>
-    <input/>
+    <Heading1 />
+    <Link />
+    <Input />
   </>);
 };
+
+const Heading1 = () => {
+  return (
+   <h1 className="Hehe">hello world</h1>
+  )
+}
+
+const Link = () => {
+  return (
+    <a href="#">ding sing</a>
+  )
+}
+
+const Input = () => {
+  return (
+    <input placeholder="heheheh"/>
+  )
+}
+
 
 
 // const Greeting = () => {
