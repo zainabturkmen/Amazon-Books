92f494b6521ae5df0a26a393efdea61a9eaf5abf
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-16 15:18:03 +0430

    adding some changes

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
  
 
-// function Greeting(){
-//   return (
-//     <div>
-//       <h1>Helo world</h1>
-//     </div>
-//   )
-// };
-
-
-const Greeting = () => {
-  return React.createElement("div", {}, React.createElement("h1", {}, "hello world"));
+function Greeting(){
+  return (
+    <div>
+      <h1>Helo world</h1>
+      <ul>
+        <li>
+          <a href="#">Hey there</a>
+        </li>
+      </ul>
+    </div>
+  )
 };
+
+
+// const Greeting = () => {
+//   return React.createElement("div", {}, React.createElement("h1", {}, "hello world"));
+// };
 
 
 ReactDOM.render(<Greeting/>, document.getElementById("root"));
