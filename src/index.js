0c25bc6ed016f268394a398de77ab329d03d7314
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-17 20:56:59 +0430

    adding some changes

--- src/index.js
+++ src/index.js
@@ -1,42 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
  
+// css
+import "./index.css"
+
+const Author = "Amelia Hepworth";
+const title = "I Love You To The Moon And Back"
+const image = 
+"https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL600_SR600,400_.jpg";
+
 
 function BookList(){
   return(
-    <section>
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
       <Book />
     </section>
   )
 };
 
-
 const Book = () => {
-  return <article>
-    <Image />
-    <Title />
-    <Author />
-  </article>
-}
-
-const Image = () => {
   return (
-    <img src="https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL600_SR600,400_.jpg" />
-  )
-};
-
-const Title = () => {
-  return(
-    <h1>I Love You To The Moon And Back</h1>
-  )
-};
-
-const Author = () => {
-  return(
-    <h4>Amelia Hepworth</h4>
+  <article className="book">
+      <img src={image} />
+      <h1>{title}</h1>
+      <h4>{Author}</h4>
+  </article>
   )
 }
+
 ReactDOM.render(<BookList />, document.getElementById("root"));
