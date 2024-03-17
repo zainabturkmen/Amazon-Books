3f1e6726471c51b30f7bcea3d9a126c3eb008c5a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-17 21:18:57 +0430

    adding some changes

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import "./index.css"
 // setup vars
 const firstBook = {
   img: "https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL600_SR600,400_.jpg",
-  
+  title: "I Love You To The Moon And Back",
+  Author: "Amelia Hepworth"
 }
 
-const Author = "Amelia Hepworth";
-const title = "I Love You To The Moon And Back"
+const Author = 
+const title =
 const image = 
 
 
