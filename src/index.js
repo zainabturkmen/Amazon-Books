77afe23375217d4b963d3ea3926e35a414debe1b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-17 22:07:06 +0430

    adding some changes

--- src/index.js
+++ src/index.js
@@ -11,11 +11,17 @@ const firstBook = {
   Author: "Amelia Hepworth"
 };
 
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81959ym88cL._AC_UL600_SR600,400_.jpg",
+  title: "Llama Llama Easter Egg",
+  Author: "Anna Dewdney"
+};
+
 function BookList(){
   return(
     <section className="booklist">
-      <Book job="developer" />
-      <Book title="random title" number={22}/>
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.Author} />
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.Author} />
     </section>
   )
 };
@@ -24,11 +30,10 @@ const Book = (props) => {
   console.log(props);
   return (
   <article className="book">
-      <img src={image} />
-      <h1>{title}</h1>
-      <h4>{Author}</h4>
+      <img src={props.img} />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
       <p>{props.job}</p>
-      <p>{props.title}</p>
       <p>{props.number}</p>
   </article>
   )
