43a501ff2537da4697a45df617c8974a721288aa
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-18 22:48:34 +0430

    adding some changes

--- src/Books.js
+++ src/Books.js
@@ -36,9 +36,33 @@ const books = [
   },
     {
     id: 6,
-    img: "https://images-na.ssl-images-amazon.com/images/I/915MRWllanL._AC_UL300_SR300,200_.jpg",
-    title: "Rhett & Link Present",
-    author: "Josh Scherer"
+    img: "https://images-na.ssl-images-amazon.com/images/I/91HHxxtA1wL._AC_UL600_SR600,400_.jpg",
+    title: "The Wonderful Things You Will Be",
+    author: "Emily Winfield Martin"
+  },
+    {
+    id: 7,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71hwUY5ZmxL._AC_UL600_SR600,400_.jpg",
+    title: "Interesting Facts For Curious Minds: 1572",
+    author: "Jordan Moore"
+  },
+    {
+    id: 8,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91Xqo-RwhfL._AC_UL600_SR600,400_.jpg",
+    title: " Little Planets: A Solar System Book for Kids",
+    author: "Chris Ferrie"
+  },
+    {
+    id: 9,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71HtAExOQjL._AC_UL600_SR600,400_.jpg",
+    title: " Flash Cards: Sight Words",
+    author: "Scholastic Teacher Resources"
+  },
+    {
+    id: 10,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81RYRl7DvXL._AC_UL600_SR600,400_.jpg",
+    title: "Bad Therapy: Why the Kids Aren't Growing Up",
+    author: "Abigail Shrier"
   },
 
 ];
