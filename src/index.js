0ec550d04b9612efd2191b5c25036b639e786770
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-18 06:58:56 +0430

    adding some changes

--- src/index.js
+++ src/index.js
@@ -5,32 +5,26 @@ import ReactDOM from "react-dom";
 import "./index.css"
 
 // setup vars
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL600_SR600,400_.jpg",
-  title: "I Love You To The Moon And Back",
-  Author: "Amelia Hepworth"
-};
+const books = [
 
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81959ym88cL._AC_UL600_SR600,400_.jpg",
-  title: "Llama Llama Easter Egg",
-  Author: "Anna Dewdney"
-};
+    {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL600_SR600,400_.jpg",
+    title: "I Love You To The Moon And Back",
+    Author: "Amelia Hepworth"
+  },
+  
+    {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81959ym88cL._AC_UL600_SR600,400_.jpg",
+    title: "Llama Llama Easter Egg",
+    Author: "Anna Dewdney"
+  }
+
+];
 
 function BookList(){
   return(
     <section className="booklist">
-      <Book 
-      img={firstBook.img} 
-      title={firstBook.title} 
-      author={firstBook.Author} 
-      >
-        <p>lorem</p>
-      </Book>
-      <Book 
-      img={secondBook.img} 
-      title={secondBook.title} 
-      author={secondBook.Author} />
+   
     </section>
   )
 };
