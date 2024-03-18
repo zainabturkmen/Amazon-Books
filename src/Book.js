ed20130da5723dc34b25517807bd5f92292b4a90
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-18 23:38:02 +0430

    adding some changes

--- src/Book.js
+++ src/Book.js
@@ -4,9 +4,7 @@ const Book = (props) => {
   const {img, title, author} = props.book
   return (
     <article className="book">
-        <a href="https://www.amazon.com/I-Love-You-Moon-Back/dp/1589255518/ref=zg_bs_g_books_d_sccl_15/145-6832180-9057112?psc=1">
-           <img src={img}/>
-        </a>
+      <img src={img}/>
       <h1>{title}</h1>
       <h4>{author}</h4>
     </article>
