2a8d69791c4a7f0c786672d1f1ea3831b56d18c8
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-23 16:12:42 +0430

    adding some features

--- src/Book.js
+++ src/Book.js
@@ -1,18 +1,5 @@
 import books from "./Books"
 
-// const onContextMenu = (event) => {
-//   event.preventDefault();
-//   const btn = document.querySelector(".btn");
-//   btn.style.display = "block"
-//   const clientRect = btn.getBoundingClientRect();
-
-//   clientRect.x = `${event.x}px`;  
-//   clientRect.y = `${event.y}px`; 
-
-//   btn.style.left = `${clientRect.x}px`;  
-//   btn.style.top = `${clientRect.y}px`;  
-// }
-
 
 const Book = (props) => {
   const {img, title, author} = props.book
