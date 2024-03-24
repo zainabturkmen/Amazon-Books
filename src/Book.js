876a62c304fffaea3aaed3361bd8c6958d027f41
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-24 08:10:18 +0430

    adding some features

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,13 @@ import { useState } from "react";
 const Book = () => {
   const [myBooks, setMyBooks] = useState(books);
   return(
-    
+    <>
+    {
+      myBooks.map((book) => {
+        const{id, img, title, author}
+      })
+    }
+    </>
   )
 };
 
