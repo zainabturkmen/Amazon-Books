8bdde6d8d793f0e74e2f89df35a307a14f55a843
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-29 10:18:39 +0430

    adding counter

--- src/index.js
+++ src/index.js
@@ -28,25 +28,7 @@ const Title = () => {
   )
 };
 
-const Counter = () => {
-  const [value, setValue] = useState(0)
 
-  const reset = () => {
-    setValue(0)
-  }
-
-  return(
-    <>
-    <section className="counter">
-      <h2>Start Counting</h2>
-      <h1>{value}</h1>
-      <button className="count-btn" onClick={() => setValue(value - 1)}>decrease</button>
-      <button className="count-btn" onClick={reset}>reset</button>
-      <button className="count-btn" onClick={() => setValue(value + 1)}>increase</button>
-    </section>
-    </>
-  )
-};
 
 ReactDOM.render(<BookList />, document.getElementById("root"))
  
